e0136559454e2dbad77aa76fda9b7608bd584fdc
arunkumar93844 <arunkumarasaithambi502@gmail.com>
2023-10-16 08:47:14 +0530

    implemnted rate limiting(throttling),Error Handling,Logging

--- data_fetch_store.py
+++ data_fetch_store.py
@@ -1,16 +1,20 @@
+from ratelimit import limits,sleep_and_retry
+import logging
 import requests
 import json
 import os
-
+logging.basicConfig(level=logging.DEBUG,filename="LogRecorder.log", format='%(asctime)s %(message)s\n',filemode='+w')
 def user_input():
-    access_token = input('Enter your access token here: ')
-    ad_account_id = input('Enter your ad account id here: ')
-    api_version = input('Enter your api version here: ')
-    folder_path = input('Enter your folder path here (preferably copy and paste the folder path): ')
-    file_name = input('Enter your filename with its .extension like "mynewpython.py" here: ')    return access_token,ad_account_id,api_version,folder_path,file_name
-    return access_token, ad_account_id, api_version, folder_path, file_name
+    logging.info('Gathering user informations: access_Token,ad_Account_Id,api_Version\n')
+    access_Token = input('Enter your access token here: ')
+    ad_Account_Id = input('Enter your ad account id here: ')
+    api_Version = input('Enter your api version here: ')
+    logging.info(f"Successfully gathered user informations:\n access_Token = {access_Token}\n ad_Account_Id = {ad_Account_Id}\n api_Version = {api_Version}\n")
+    return access_Token,ad_Account_Id,api_Version
 
-def basic_data_fetch(access_token,ad_account_id,api_version):
+@sleep_and_retry
+@limits(calls=1,period=18)
+def data_fetch(access_token,ad_account_id,api_version):
     graph_api_url = f'https://graph.facebook.com/v{api_version}/{ad_account_id}'
     feilds={'access_token':access_token,
             'ad_accounts',
@@ -18,31 +22,52 @@ def basic_data_fetch(access_token,ad_account_id,api_version):
             'ads{id,name,creative,insights,targeting}',
             'campaigns{id,name,start_time,end_time,creative,insights,targeting}'
             }
-    response_raw = requests.get(url= graph_api_url, params= feilds)
-    return response_raw
-
-def data_process_store(response_raw,folder_path,file_name):
-    response_json_raw = response_raw.json()
-    response_json =json.dumps(response_json_raw,indent=4)
-    with open(f'{folder_path}\{file_name}','a+') as file:
-        json.dump(response_json,file)
+    try:
+        logging.info("Making API request...\n") 
+        response_raw = requests.get(url= graph_api_url, params= feilds)
+        response_raw.raise_for_status()
+        logging.info("API request was successfull and Informations were fetched...\n")
+        return response_raw
     
+    except requests.exceptions.RequestException as error:
+        logging.exception(f'API request failed:{str(error)}\n Skipping information processing and storing...\n')
+        print(f'API request failed:{str(error)}')
+        return False
+
+def data_process_store(response):
+        if response:
+            folder_path = input('Enter your folder path where you file should be created (remove "" while pasting): ')
+            file_name = input('Enter your filename with its .extension like "mynewpython.py" here: ')
+
+            try:
+                response_json = response.json()
+                logging.info("Information is proccessed into JSON format...\n")
+                with open(f'{folder_path}\\{file_name}','a+') as file:
+                    file.seek(0)
+                    json.dump(response_json,file,indent=4)
+                    logging.info(f"Information is stored at the location {folder_path}\\{file_name}...\n")
+                    file.close()
+            except json.JSONDecodeError as error:
+                logging.exception(f'Failed to parse JSON response: {str(error)...\n'}
+                print(f"Failed to parse JSON response: {str(error)}")
+                return True
+            except OSError as error:
+                logging.exception(f'Information storing failed:{str(error)}...\n')
+                print(f'Error Occured:{str(error)}\nEnter the file path correctly.')
+                data_process_store(response)
+                logging.info(f'Information is successfully stored in {file_name}...\n')
+                return True
             
-def main(response_raw):
-    
-    access_token, ad_account_id, api_version, folder_path, file_name= user_input()
-    basic_data_fetch(access_token,ad_account_id,api_version)
-    if response_raw.status_code == 200:
-        data_process_store(response_raw,folder_path,file_name)
-    else:
-        print('Request failed')
-                
-main()
+def main():
+    logging.info('FB_Manager has started...\n')
+    access_token, ad_account_id, api_version= user_input()
+    response= data_fetch(access_token,ad_account_id,api_version)
+    data_process_store(response)
+    logging.info('FB_Manager has stoped...\n\n')
+    return True
 
-    
+if __name__=="__main__":
+    main()
 
 
-
-
-    
 
